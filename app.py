fda0c2eb4c81fa90d87af7590e8830ac6738cae3
bahromomonov97 <94924148+bahromomonov@users.noreply.github.com>
2022-09-15 21:49:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -14,3 +14,5 @@ if file:
     pred,pred_id,probs=model.predict(img)
     st.success(f"Bashorat:{pred}")
     st.info(f"Ehtimollik:{probs[pred_id]*100:1f}")
+    fig=px.bar(x=probs*100,y=model.dls.vocab)
+    st.plotly_chart(fig)
